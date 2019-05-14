3b857d3f5fdc7cfee90f9b2556b9e6286720a28b
yangxiaona <yangxiaona@qiushibaike.com>
2019-05-14 17:10:53 +0800

    opt:add crawler

---
+++ crawler/trace.js
@@ -0,0 +1,13 @@
+const puppeteer = require('puppeteer');
+const devices = require('puppeteer/DeviceDescriptors');
+const iPhone = devices['iPhone 6'];
+
+(async () => {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.emulate(iPhone); // 让页面模拟成iphone6
+  await page.tracing.start({ path: 'test/doc/trace.json' }); // 生成页面性能追踪的文件
+  await page.goto('https://toutiao.qiushibaike.com/yuedu/taskcenter');
+  await page.tracing.stop();
+  browser.close();
+})();
---
+++ crawler/uitest.js
@@ -0,0 +1,33 @@
+
+
+
+const puppeteer = require('puppeteer');
+const devices = require('puppeteer/DeviceDescriptors');
+const iPhone = devices['iPhone 6'];
+(async () => {
+  const browser = await puppeteer.launch({ headless: false, slowMo: 0 });
+  const page = await browser.newPage();
+  await page.emulate(iPhone); // 让页面模拟成iphone6
+  await page.goto('https://m.v.qq.com/play.html?cid=rjvr8psrvic4567&vid=')
+  await page.screenshot({ path: 'test/image/1.png' })
+
+  await page.click('.btn_user_text')
+  await page.waitFor(1000)
+  await page.screenshot({ path: 'test/image/2.png' })
+
+  await page.waitFor(1000)
+  await page.click('.btn_qq')
+  await page.waitFor(1000)
+  await page.screenshot({ path: 'test/image/3.png' })
+  await page.type('#u', '******') // 账号
+  await page.type('#p', '******') // 密码
+  await page.screenshot({ path: 'test/image/4.png' })
+  await page.waitFor(1000)
+
+  await page.click('#go')
+  await page.waitFor(1000)
+  await page.screenshot({ path: 'test/image/5.png' })
+  await page.waitFor(1000)
+
+  await browser.close();
+})()
